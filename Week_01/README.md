00dc0bb2aead2829235c33778ea20ff7f95d282a
zhaimy <mengying.zhai@foresight-robotics.com>
2020-06-25 16:57:57 +0800

    week 1 solutions 1

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,3 @@
 学习笔记
+
+![要点](F:\algorithm011-class01\Week_01\refs\points.png)
---
+++ Week_01/solutions.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode.cn id=26 lang=python3
+#
+# [26] 删除排序数组中的重复项
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+
+        return j + 1
+
+# @lc code=end
+
+
+#
+# @lc app=leetcode.cn id=189 lang=python3
+#
+# [189] 旋转数组
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n - k - 1)
+        self.reverse(nums, n - k, n - 1)
+        self.reverse(nums, 0, n - 1)
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+# @lc code=end
+
+
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+            d[n] = i
+
+# @lc code=end
+
