6e4c5284c9160d7264a0e460aa9a624c6871792e
zhaimy <mengying.zhai@foresight-robotics.com>
2020-08-06 17:05:38 +0800

    week 7

--- Week_07/README.md
+++ Week_07/README.md
@@ -1,1 +1,74 @@
-学习笔记
+[TOC]
+
+# Trie 树
+
+应用：前缀感应词
+
+别名：字典树、单词查找树、键树
+
+特点：统计、排序字符串，查找效率比哈希表高
+
+性质：
+
+- 结点本身不存完整单词
+- 从根结点到某一结点，路径上经过的字符连接起来，为该结点对应的字符串
+- 每个结点的所有子结点路径代表的字符都不相同
+
+思想：空间换时间
+
+代码模板：
+
+```python
+class Trie(object):
+  
+	def __init__(self): 
+		self.root = {} 
+		self.end_of_word = "#" 
+ 
+	def insert(self, word): 
+		node = self.root 
+		for char in word: 
+			node = node.setdefault(char, {}) 
+		node[self.end_of_word] = self.end_of_word 
+ 
+	def search(self, word): 
+		node = self.root 
+		for char in word: 
+			if char not in node: 
+				return False 
+			node = node[char] 
+		return self.end_of_word in node 
+ 
+	def startsWith(self, prefix): 
+		node = self.root 
+		for char in prefix: 
+			if char not in node: 
+				return False 
+			node = node[char] 
+		return True
+```
+
+
+
+# 并查集
+
+应用：组团、配对，与 group 有关
+
+```python
+def init(p): 
+	p = [i for i in range(n)] 
+ 
+def union(self, p, i, j): 
+	p1 = self.parent(p, i) 
+	p2 = self.parent(p, j) 
+	p[p1] = p2 # 将一个集合老大的父节点设置为另一个集合老大
+ 
+def parent(self, p, i): # 查找到集合老大
+	root = i 
+	while p[root] != root: 
+		root = p[root] 
+	while p[i] != i: # 路径压缩，不影响函数结果，但可优化查找集合老大的时间，缩短到O(1)
+		x = i; i = p[i]; p[x] = root 
+	return root
+```
+
---
+++ Week_07/solutions.py
@@ -0,0 +1,90 @@
+#
+# @lc app=leetcode.cn id=208 lang=python3
+#
+# [208] 实现 Trie (前缀树)
+#
+
+# @lc code=start
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.end_of_word = "#"
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for ch in word:
+            node = node.setdefault(ch, {}) # key 为空则设置，否则取值
+        node[self.end_of_word] = self.end_of_word
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for ch in word:
+            if ch not in node: return False
+            node = node[ch]
+        return self.end_of_word in node
+        
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for ch in prefix:
+            if ch not in node: return False
+            node = node[ch]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
+
+
+#
+# @lc app=leetcode.cn id=547 lang=python3
+#
+# [547] 朋友圈
+#
+
+# @lc code=start
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        if not M: return 0
+
+        n = len(M)
+        p = [i for i in range(n)] # 创建并查集
+
+        for i in range(n):
+            for j in range(n):
+                if M[i][j] == 1:
+                    self._union(p, i, j) # 合并
+        
+        return len(set([self._parent(p, i) for i in range(n)]))
+
+    def _union(self, p, i, j):
+        p1 = self._parent(p, i)
+        p2 = self._parent(p, j)
+        p[p2] = p1
+
+    def _parent(self, p, i):
+        root = i
+        while p[root] != root:
+            root = p[root]
+        while p[i] != i:
+            tmp = i; i = p[i]; p[tmp] = root
+        return root
+
+# @lc code=end
+
+
